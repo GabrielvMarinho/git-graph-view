707a3b4009d5a7134e9f32d465ee5d285b187d94
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-07-22 19:23:24 -0300

    added test for checking if is still in current branch after commit

--- commit.test.js
+++ commit.test.js
@@ -8,6 +8,9 @@ test("git commit", () =>{
     expect(response).toBe(`[main ${newSha}]`)
     expect(newSha).not.toBe(firstSha)
 
+    currentBranch = cmdDisp.commandManager.gitObject.getCurrentBranch()
+    expect(currentBranch.name).toBe("main")
+
     cmdDisp.receiveAndDispatchCommand("git checkout "+newSha)
     response = cmdDisp.receiveAndDispatchCommand("git commit")
     newSha = cmdDisp.commandManager.gitObject.getCurrentHash().slice(0, 7)
@@ -21,6 +24,9 @@ test("git commit -m 'message'", () =>{
     newSha = cmdDisp.commandManager.gitObject.getCurrentHash().slice(0, 7)
     expect(response).toBe(`[main ${newSha}] this is a message`)
     expect(newSha).not.toBe(firstSha)
+
+    currentBranch = cmdDisp.commandManager.gitObject.getCurrentBranch()
+    expect(currentBranch.name).toBe("main")
 
     cmdDisp.receiveAndDispatchCommand("git checkout "+newSha)
     response = cmdDisp.receiveAndDispatchCommand("git commit -m 'new message'")
