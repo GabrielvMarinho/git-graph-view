f4c22717da506d4c5fa5e66894dff5a7640404cf
marinho <gabrielvmarinho1711@gmail.com>
2025-09-07 22:49:56 -0300

    added repo link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import GitGraph from "./components/GitGraph.js";
 import "./styles.css"
 export default function App() {
   return (
-      <GitGraph></GitGraph>
+      <>
+        <a href='https://github.com/GabrielvMarinho/git-graph-view/' className='repositoryButton'>Click here for the public repository!</a>
+        <GitGraph></GitGraph>
+      </>
   );
 };
